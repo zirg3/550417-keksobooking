ca220c3a31c0eca89a7bcdb62ff28da45bd8ea9e
zirg3 <zirg31@gamil.com>
2018-12-24 20:43:57 +0300

    Валидация

--- js/map.js
+++ js/map.js
@@ -153,7 +153,7 @@ var renderCard = function (appartment) {
   return cardElement;
 };
 
-// var filter = document.querySelector('.map__filters-container');
+// var filter = document.querySelector('.map__filters-container');-----------------------------
 var map = document.querySelector('.map');
 
 //  Выключает формы
@@ -236,4 +236,109 @@ document.addEventListener('keydown', function (evt) {
   if (evt.keyCode === ESC__KEY) {
     closeCard();
   }
+});
+
+// Тип - плата-------------------------------------------------------------------------------
+var inputPrice = document.querySelector('#price');
+var typeHous = document.querySelector('#type');
+
+var typePrice = {
+  'bungalo': {
+    min: '0',
+    placeholder: '0'
+  },
+  'flat': {
+    min: '1000',
+    placeholder: '1000'
+  },
+  'house': {
+    min: '5000',
+    placeholder: '5000'
+  },
+  'palace': {
+    min: '10000',
+    placeholder: '10000'
+  }
+};
+
+var synchPrice = function () {
+  switch (typeHous.value) {
+    case 'bungalo':
+      inputPrice.min = typePrice.bungalo.min;
+      inputPrice.placeholder = typePrice.bungalo.placeholder;
+      return;
+    case 'flat':
+      inputPrice.min = typePrice.flat.min;
+      inputPrice.placeholder = typePrice.flat.placeholder;
+      return;
+    case 'house':
+      inputPrice.min = typePrice.house.min;
+      inputPrice.placeholder = typePrice.house.placeholder;
+      return;
+    case 'palace':
+      inputPrice.min = typePrice.palace.min;
+      inputPrice.placeholder = typePrice.palace.placeholder;
+      return;
+  }
+};
+synchPrice();
+
+typeHous.addEventListener('input', synchPrice);
+// Комнаты-места
+var inputCapacity = document.querySelector('#capacity');
+var inputRoomNumber = document.querySelector('#room_number');
+var capacityOptions = inputCapacity.querySelectorAll('option');
+var roomNumberOptions = inputRoomNumber.querySelectorAll('option');
+
+var synch = function () {
+  for (var i = 0; i < capacityOptions.length; i++) {
+    capacityOptions[i].disabled = false;
+  }
+};
+
+var roomsAndPlaces = function () {
+  synch();
+  for (var i = 0; i < roomNumberOptions.length; i++) {
+    if (roomNumberOptions[i].selected === true) {
+      switch (roomNumberOptions[i].value) {
+        case '1':
+          capacityOptions[0].disabled = true;
+          capacityOptions[1].disabled = true;
+          capacityOptions[2].selected = true;
+          capacityOptions[3].disabled = true;
+          break;
+        case '2':
+          capacityOptions[0].disabled = true;
+          capacityOptions[1].selected = true;
+          capacityOptions[3].disabled = true;
+          break;
+        case '3':
+          capacityOptions[0].selected = true;
+          capacityOptions[3].disabled = true;
+          break;
+        case '100':
+        capacityOptions[0].disabled = true;
+        capacityOptions[1].disabled = true;
+        capacityOptions[2].disabled = true;
+        capacityOptions[3].selected = true;
+          break;
+      }
+    }
+  }
+};
+roomsAndPlaces();
+
+
+inputRoomNumber.addEventListener('change', roomsAndPlaces);
+
+// Время заезда и выезда
+var timein = document.querySelector('#timein');
+var timeout = document.querySelector('#timeout');
+
+timein.addEventListener('change', function (evt) {
+  timeout.value = evt.target.value;
+});
+
+timeout.addEventListener('change', function (evt) {
+  timein.value = evt.target.value;
 });
